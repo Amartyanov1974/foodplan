f5debe52755b5b59bc1bfc3a9e4df7e494b28e06
Amartyanov1974 <randomquake@yandex.ru>
2023-09-25 23:21:05 +0400

    подключил статику

--- food_plan/views.py
+++ food_plan/views.py
@@ -1,9 +1,34 @@
 from django.shortcuts import render
 
+
 def index(request):
     context = {}
     return render(request, 'index.html',)
 
-def login(request):
+def lk(request):
     context = {}
-    return render(request, 'index.html', )
+    return render(request, 'lk.html', )
+
+def auth(request):
+    context = {}
+    return render(request, 'auth.html', )
+
+def registration(request):
+    context = {}
+    return render(request, 'registration.html', )
+
+def order(request):
+    context = {}
+    return render(request, 'order.html', )
+
+def card1(request):
+    context = {}
+    return render(request, 'card1.html', )
+
+def card2(request):
+    context = {}
+    return render(request, 'card2.html', )
+
+def card3(request):
+    context = {}
+    return render(request, 'card3.html', )
