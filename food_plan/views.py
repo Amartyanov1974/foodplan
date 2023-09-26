e282a6c88c9d3e2799969599f55b17bef4dc64fb
Amartyanov1974 <randomquake@yandex.ru>
2023-09-26 17:16:08 +0400

    Сделал регистрацию на сайте

--- food_plan/views.py
+++ food_plan/views.py
@@ -1,25 +1,69 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login,logout, authenticate
+from django.contrib.auth.models import User
 
 
 def index(request):
-    context = {}
-    return render(request, 'index.html',)
+    if 'user_name' in request.session:
+        username = request.session['user_name']
+        context = {
+            'username': username,
+            }
+    return render(request, 'index.html', context=context)
 
 def lk(request):
     context = {}
     return render(request, 'lk.html', )
 
 def auth(request):
+
     context = {}
     return render(request, 'auth.html', )
 
+def deauth(request):
+    logout(request)
+    request.session['user_name'] = ''
+    return redirect('/')
+
+def registration_message(request):
+    context = {
+        'message': request.session.get('message'),
+        }
+    request.session['message'] =''
+    return render(request, 'registration.html', context=context)
+
 def registration(request):
-    context = {}
-    return render(request, 'registration.html', )
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['passwd']
+        passwordconfirm = request.POST['passwdconfirm']
+        email = request.POST['email']
+        if password != passwordconfirm:
+            request.session['message'] = 'Пароли не совпадают'
+            return redirect('registration_message')
+        try:
+            user = authenticate(username=username, password=password)
+        except:
+            user=None
+        if not user:
+            try:
+                user = User.objects.create_user(
+                    username=username,
+                    password=password,
+                    email=email,
+                )
+            except:
+                request.session['message'] =  'Пользователь с таким именем существует'
+                return redirect('registration_message')
+        login(request, user)
+        request.session['user_name'] = username
+        return redirect('/')
+
+    return render(request, 'registration.html' )
 
 def order(request):
     context = {}
-    return render(request, 'order.html', )
+    return render(request, 'order.html' )
 
 def card1(request):
     context = {}
