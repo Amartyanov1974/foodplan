b934df219ae8038e2bed2ed063b929a24d8f58c6
Michael Matasyants <michael.matasyanc@gmail.com>
2023-09-28 18:40:51 +0300

    Display models in admin

--- food_plan/admin.py
+++ food_plan/admin.py
@@ -1,5 +1,63 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from adminsortable2.admin import SortableStackedInline, SortableAdminBase
+from food_plan.models import (Client, MealPlan, FoodList, Foodstuff,
+                              Recipe, DishType, Image, Description)
 
-# Register your models here.
 
-# Add adminsortable2.admin to sort images and description with drag
+
+class ImageInline(SortableStackedInline):
+    '''Inline for images to show in Place model'''
+    model = Image
+    readonly_fields = ['preview']
+
+    def preview(self, obj):
+        '''display preview of image'''
+        return format_html('<img src="{}" width="200" height="200" />',
+                           obj.image.url)
+
+
+@admin.register(Recipe)
+class RecipeAdmin(SortableAdminBase, admin.ModelAdmin):
+    list_display = ['menu_type', 'cooking_time', 'calories',
+                    'fats', 'proteins', 'carbs']
+    inlines = [
+        ImageInline,
+    ]
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    '''Admin panel for Image model'''
+    pass
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ['user', 'subscription', 'subscription_expiration_date',
+                    'meal_plan']
+
+
+@admin.register(FoodList)
+class FoodListAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(DishType)
+class DishTypeAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+@admin.register(Description)
+class DescriptionAdmin(admin.ModelAdmin):
+    list_display = ['recipe', 'text', 'text_position']
+
+
+@admin.register(Foodstuff)
+class FoodstuffAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'weight']
+
+
+@admin.register(MealPlan)
+class MealPlanAdmin(admin.ModelAdmin):
+    list_display = ['menu_type', 'number_persons', 'allergies',
+                    'calories', 'number_of_meals']
--- food_plan/models.py
+++ food_plan/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.core import validators
 from django.contrib.auth.models import User
 
 
